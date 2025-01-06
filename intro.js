fba07582786dcf8df746cfe73f22a47f419f6fa7
EndriusOrdonhes <ordonhesgomesendrius@gmail.com>
2025-01-06 12:11:58 -0300

    Add files via upload

---
+++ intro.js
@@ -0,0 +1,39 @@
+console.log("ola")
+console.log("Endrius","Endrius","Endrius","Endrius")
+
+//VARIAVEIS
+//responsaveis por guardar dados na memoria
+
+var nome = "Endrius"
+
+let sobrenome = "Silva"
+const verdadeiro = true
+const idade = 18
+var lanchonete = "Mc Donald"
+
+// tipos dos dados
+// string, number boolean
+
+console.log(nome, nome, nome, nome)
+console.log(2)
+console.log(lanchonete, typeof lanchonete)
+console.log(idade, typeof idade)
+console.log(verdadeiro, typeof verdadeiro)
+
+//concatenação
+console.log(nome+ " " + sobremesa)
+
+//conta com variavel
+
+var preco = 25
+var quantidade = 3
+var total = preco * quantidade
+
+console.log(total, 'total da conta com variavel')
+
+// virgula para separar variaveis delcaradas ao mesmo tempo
+var loja, cidade, numero;
+loja = "Tunel do Rock"
+cidade = "Curitiba"
+numero = 339
+console.log (loja, cidade)
